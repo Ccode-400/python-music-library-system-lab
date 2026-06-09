bfa004bdf31207b45f0b3346f5baef43f975a997
Ccode-400 <stanleymngige@gmail.com>
2026-06-09 21:18:04 +0300

    added add_to_genre_count method

--- lib/song.py
+++ lib/song.py
@@ -28,6 +28,13 @@ class Song:
     @classmethod
     def add_to_artists(cls, artist):  
         if artist not in cls.artists:
-            cls.artists.append(artist)   
+            cls.artists.append(artist) 
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1  
 
         
